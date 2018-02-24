8232738c4378c1edd2f50518fa56033c84c6dcd7
hecter <hecter654321@gmail.com>
2018-02-24 22:46:01 +0800

    pass testcase: testSameple by using for statement

--- Best Time to Buy and Sell Stock II.py
+++ Best Time to Buy and Sell Stock II.py
@@ -5,11 +5,18 @@ class unitest(unittest.TestCase):
         Input = []
         Output = 0
         self.assertEqual(Solution().maxProfit(Input),Output)
+    def testSample(self):
+        Input = [1,2,3,4,5]
+        Output = 4
+        self.assertEqual(Solution().maxProfit(Input),Output)
 
 class Solution():
     def maxProfit(self, prices):
-        if prices == []:
-            return 0
+        total = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                total += prices[i+1] - prices[i]
+        return total
 
 if __name__ == '__main__':
     unittest.main()
